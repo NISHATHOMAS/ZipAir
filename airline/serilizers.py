706742a97a877e208d5f34d768118a555e81d0be
Abel Abi <niz.shabzt@gmail.com>
2022-01-23 20:18:43 +0530

    save instance

--- airline/serilizers.py
+++ airline/serilizers.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import math
 from decimal import Decimal
 from rest_framework import serializers
@@ -32,11 +34,20 @@ class CreateAirLineSerializer(serializers.ModelSerializer):
         return self.total_fuel_consumption
 
     def get_maximum_travel_time(self, obj):
-        self.maximum_travel_time = round((self.fuel_capacity/self.total_fuel_consumption), 3)
+        self.maximum_travel_time = timedelta(minutes=round((self.fuel_capacity/self.total_fuel_consumption), 3))
         return self.maximum_travel_time
 
     def create(self, validated_data):
         airline_obj = AirLine(**validated_data)
+        validated_data["fuel_capacity"] = self.get_fuel_capacity(airline_obj)
+        validated_data["passenger_fuel_consumption"] = self.get_passenger_fuel_consumption(airline_obj)
+        validated_data["airline_fuel_consumption"] = self.get_airline_fuel_consumption(airline_obj)
+        validated_data["total_fuel_consumption"] = self.get_total_fuel_consumption(airline_obj)
+        validated_data["maximum_travel_time"] = self.get_maximum_travel_time(airline_obj)
+        print("validated_data", validated_data["airline_id"], validated_data["total_fuel_consumption"])
+        airline_obj = AirLine(**validated_data)
+        print(airline_obj)
+        airline_obj.save()
         return airline_obj
 
     def __init__(self, *args, **kwargs):
